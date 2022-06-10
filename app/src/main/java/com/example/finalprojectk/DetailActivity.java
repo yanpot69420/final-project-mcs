65e1dc985a0a24743931f550f58b4a0cc1f57d8b
yanpot69420 <reyhanahmad2002@gmail.com>
2022-06-10 16:02:08 +0800

    finishing DetailActivity 2

--- app/src/main/java/com/example/finalprojectk/DetailActivity.java
+++ app/src/main/java/com/example/finalprojectk/DetailActivity.java
@@ -1,9 +1,11 @@
 package com.example.finalprojectk;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -11,26 +13,30 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.finalprojectk.object.Product;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener{
     ImageView detailImage;
-    TextView detailName;
-    TextView detailPrice;
-    TextView detailDescription;
+    TextView detailName, detailPrice, detailDescription, detailQuantity;
+    Button btnIncrease, btnDecrease, btnBuy;
+    AlertDialog.Builder buyNotification;
     RatingBar detailRating;
+    Integer quantity = 0;
+    Product product;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         initView();
-        Product product = (Product) getIntent().getSerializableExtra("detail");
+        product = (Product) getIntent().getSerializableExtra("detail");
+        buyNotification = new AlertDialog.Builder(this);
         if(product!=null){
             initVar(product);
-//            Log.wtf("productlist", product.getProductName());
-//            Log.wtf("productlist", product.getProductDescription());
-//            Log.wtf("productlist", product.getProductImage());
-
         }
+        initBuyBuilder(buyNotification);
+        btnIncrease.setOnClickListener(this);
+        btnDecrease.setOnClickListener(this);
+        btnBuy.setOnClickListener(this);
     }
 
     void initView(){
@@ -39,6 +45,10 @@ public class DetailActivity extends AppCompatActivity {
         detailRating = findViewById(R.id.detailRating);
         detailPrice = findViewById(R.id.detailPrice);
         detailDescription = findViewById(R.id.detailDescription);
+        btnBuy = findViewById(R.id.btnBuy);
+        btnIncrease = findViewById(R.id.btnIncrease);
+        btnDecrease = findViewById(R.id.btnDecrease);
+        detailQuantity = findViewById(R.id.detailQuantity);
     }
 
     void initVar(Product product){
@@ -47,5 +57,43 @@ public class DetailActivity extends AppCompatActivity {
         detailRating.setRating(product.getProductRating());
         detailPrice.setText("Price : $" +product.getProductPrice().toString());
         detailDescription.setText("Description :\n"+product.getProductDescription());
+    }
+
+    void initBuyBuilder(AlertDialog.Builder builder){
+
+        builder.setIcon(R.drawable.black_cart)
+                .setTitle("Are you sure you wanna buy this item for 69$?")
+                .setCancelable(true)
+                .setPositiveButton("Yes", (dialogInterface, i) -> {
+                    Toast.makeText(this, "Terbeli coy", Toast.LENGTH_SHORT).show();
+                    quantity = 0;
+                    detailQuantity.setText(String.valueOf(quantity));
+
+                })
+                .setNegativeButton("No", (dialogInterface, i) -> {
+                    dialogInterface.cancel();
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnIncrease:
+                quantity++;
+                detailQuantity.setText(String.valueOf(quantity));
+                break;
+            case R.id.btnDecrease:
+                if(quantity>0)
+                quantity--;
+                detailQuantity.setText(String.valueOf(quantity));
+                break;
+            case R.id.btnBuy:
+                if(quantity<1)
+                    Toast.makeText(this, "Quantity must be greater than 0", Toast.LENGTH_SHORT).show();
+                else {
+                    buyNotification.show();
+                }
+                break;
+        }
     }
 }
